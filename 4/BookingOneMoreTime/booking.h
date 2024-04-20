efce2ab6b361635666a2e1eebc03803249ee929e
Evgeny <zhekatkachenko@ya.ru>
2024-04-20 10:37:14 +0300

    Booking update.

--- 4/BookingOneMoreTime/booking.h
+++ 4/BookingOneMoreTime/booking.h
@@ -5,12 +5,34 @@ namespace RAII {
 template <typename Provider>
 class Booking {
 private:
-    Provider* provider;
+    Provider* provider = nullptr;
     int counter = 0;
 public:
-    Booking();
+    Booking(Provider* pr, int c) 
+    : provider(pr),
+      counter(c)
+      {}
 
-    ~Booking();
+    Booking(const Booking&) = delete;
+    Booking& operator= (const Booking&) = delete;
+
+    Booking(Booking&& other) 
+    : provider(other.provider),
+      counter(other.counter)
+    {
+        other.provider = nullptr;
+    }
+
+    Booking& operator= (Booking&& other) {
+        std::swap(provider, other.provider);
+        std::swap(counter, other.counter);
+        return(*this);
+    }
+
+    ~Booking() {
+        if (provider)
+            provider->CancelOrComplete(*this);
+    }
 };
 
 }
